c5a87c80a904be2283de24c408d0cf04efa7cabe
PhillTartari92 <99393455+PhillTartari92@users.noreply.github.com>
2023-02-16 14:23:17 +0100

    Add files via upload

---
+++ words.js
@@ -0,0 +1,8 @@
+export const wordList = {
+    voiture : ["Moteur", "Porte","Vitesse", "Roue", "Phare"],
+    fruit : ["Banana","Pomme","Poire", "Citron","Orange"],
+    corps : ["Bras", "Jambe","Yeux", "Cou","Bouche"],
+    informatique : ["Souris","Clavier","Moniteur","Webcam"],
+    programmation : ["Langage","Framework","JavaScript","React"],
+    nourriture : ["Riz","Haricot","Viande","Lait","Oeufs"]
+};
